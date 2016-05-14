ce1a363c7279ec3d9d4205f4973f8f6bdc4a2913
idnoble <idowu4bello@gmail.com>
2016-05-14 21:10:30 +0300

    update the README file

---
+++ druid-assignment/sites/all/themes/bellodruid/js/equalheight.js
@@ -0,0 +1,13 @@
+$.fn.equalHeights = function(px) {
+	$(this).each(function(){
+		var currentTallest = 0;
+		$(this).children().each(function(i){
+			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+		});
+    if (!px && Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
+		// for ie6, set height since min-height isn't supported
+		if (typeof(document.body.style.minHeight) === "undefined") { $(this).children().css({'height': currentTallest}); }
+		$(this).children().css({'min-height': currentTallest});
+	});
+	return this;
+};
---
+++ druid-assignment/sites/all/themes/bellodruid/js/pxToEm.js
@@ -0,0 +1,22 @@
+$.fn.toEm = function(settings){
+	settings = jQuery.extend({
+		scope: 'body'
+	}, settings);
+	var that = parseInt(this[0],10),
+		scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+		scopeVal = scopeTest.height();
+	scopeTest.remove();
+	return (that / scopeVal).toFixed(8) + 'em';
+};
+
+
+$.fn.toPx = function(settings){
+	settings = jQuery.extend({
+		scope: 'body'
+	}, settings);
+	var that = parseFloat(this[0]),
+		scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+		scopeVal = scopeTest.height();
+	scopeTest.remove();
+	return Math.round(that * scopeVal) + 'px';
+};
